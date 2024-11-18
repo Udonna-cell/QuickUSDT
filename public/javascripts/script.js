9ab255ede693ab2512b8c42ddcb60969a8241c48
udonna-cell <orabuezestanley@gmail.com>
2024-11-18 13:30:47 +0100

    fixed alert

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,58 +9,64 @@ btn.addEventListener("click", (e) => {
       text: "Do you want to lose your reward",
       icon: "warning",
     });
-    
   } else {
-    fetch("https://quickusdt.onrender.com/payment", {
-      method: "POST",
-      body: JSON.stringify({
-        address: address.value,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => {
-        // Check if the response is OK (status code 200-299)
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Parse the JSON from the response
-      })
-      .then((data) => {
-        console.log(data); // Now `data` is the parsed JSON object
-        if (data.status == true) {
-          Swal.fire({
-            title: "Claimed",
-            text: data.message,
-            icon: "success",
-          }).then(d=>{
-            window.location.reload()
+    Swal.fire({
+      title: "Transaction processing",
+      text: "Please wait while we make payment :)",
+      icon: "info",
+      showConfirmButton: false,
+      timer: 1500
+    }).then((value) => {
+      if (value) {
+        fetch("https://quickusdt.onrender.com/payment", {
+          method: "POST",
+          body: JSON.stringify({
+            address: address.value,
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        })
+          .then((response) => {
+            // Check if the response is OK (status code 200-299)
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json(); // Parse the JSON from the response
+          })
+          .then((data) => {
+            console.log(data); // Now `data` is the parsed JSON object
+            if (data.status == true) {
+              Swal.fire({
+                title: "Claimed",
+                text: data.message,
+                icon: "success",
+              }).then((d) => {
+                window.location.reload();
+              });
+            } else {
+              Swal.fire({
+                title: "Failed to claim",
+                text: data.message,
+                icon: "error",
+              }).then((d) => {
+                window.location.reload();
+              });
+            }
+            // Show the object as a string in the Swal.fire
+          })
+          .catch((error) => {
+            console.error(
+              "There was a problem with the fetch operation:",
+              error
+            );
+            Swal.fire({
+              title: "Claim error",
+              text: "Failed to claim please try again",
+              icon: "error",
+            });
           });
-          
-        } else {
-          Swal.fire({
-            title: "Failed to claim",
-            text: data.message,
-            icon: "error",
-          }).then(d=>{
-            window.location.reload()
-          });
-        }
-        // Show the object as a string in the Swal.fire
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-        Swal.fire({
-          title: "Claim error",
-          text: "Failed to claim please try again",
-          icon: "error",
-        });
-      });
-      Swal.fire({
-        title: "Transaction processing",
-        text: "Please wait while we make payment :)",
-        icon: "error",
-      });
+      }
+    });
   }
 });
